bb823250401d42f902a714605105c2a3b9ddb7d0
naresh-mishra <nareshmishra8535@gmail.com>
2026-06-12 17:04:14 +0530

    four

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask Backend Running Changes-1-2-3"
+    return "Flask Backend Running Changes-1-2-3-4"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
